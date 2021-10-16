630aa7bef9a3c7f0cc3853a7664321af85b4e7bc
Mark Schofield <mschofie@outlook.com>
2021-10-15 21:58:33 -0700

    Add a console, CppWinRT 1.0 example

---
+++ example/CommandLineWinRT/main.cpp
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------------------------------------------------------------
+//
+//---------------------------------------------------------------------------------------------------------------------
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Web.Syndication.h>
+#include <iostream>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Web::Syndication;
+
+int main()
+{
+    winrt::init_apartment();
+
+    Uri rssFeedUri{ L"https://blogs.windows.com/feed" };
+    SyndicationClient syndicationClient;
+    SyndicationFeed syndicationFeed = syndicationClient.RetrieveFeedAsync(rssFeedUri).get();
+    for (SyndicationItem syndicationItem : syndicationFeed.Items())
+    {
+        winrt::hstring titleAsHstring = syndicationItem.Title().Text();
+        std::wcout << titleAsHstring.c_str() << std::endl;
+    }
+}
---
+++ example/CommandLineWinRT/pch.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <iostream>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Web.Syndication.h>
